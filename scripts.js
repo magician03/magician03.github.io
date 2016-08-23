e2f917a238aea31bf2cbbc1815c8c1ca7f182e1f
magician03 <diwakar03121997@gmail.com>
2016-08-23 16:56:15 +0530

    unsafe script issue-3

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 
+$(function() {
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 500) {
+            $(".header").addClass('smaller');
+        } else {
+            $(".header").removeClass("smaller");
+        }
+    });
+});
